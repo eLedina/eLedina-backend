dd1be838ad9c1043cc85897a9b6d4a8d11df32e1
Grimpy101 <gorupgorazd@gmail.com>
2018-06-11 15:01:27 +0200

    Spremembe linkov

--- app.py
+++ app.py
@@ -12,49 +12,49 @@ def index():
 	
 	
 # design_page.html
-@app.route("/design")
+@app.route("/design.html")
 def designPage():
     return render_template("design_page.html", ip=request.remote_addr)
 	
 	
 # login.html
-@app.route("/login")
+@app.route("/login.html")
 def login():
     return render_template("login.html", ip=request.remote_addr)
 	
 	
 # register.html
-@app.route("/register")
+@app.route("/register.html")
 def register():
-    return render_template("registration.html", ip=request.remote_addr)
+    return render_template("register.html", ip=request.remote_addr)
 	
 	
 # home.html
-@app.route("/home")
+@app.route("/home.html")
 def home():
     return render_template("home.html", ip=request.remote_addr)
 	
 	
 # Blog stuff
-@app.route("/blog_main")
+@app.route("/blog_main.html")
 def blogMain():
     return render_template("blog_mainpage.html", ip=request.remote_addr)
 	
-@app.route("/blog_writing")
+@app.route("/blog_writing.html")
 def blogWrite():
     return render_template("blog_writingpage.html", ip=request.remote_addr)
 
-@app.route("/blogtemp")
+@app.route("/blogtemp.html")
 def blogTemp():
     return render_template("blogs/blog_sample.html", ip=request.remote_addr)
 	
 
 # oral_exam.html
-@app.route("/oral_exam")
+@app.route("/oral_exam.html")
 def oralExam():
     return render_template("oral_exam.html", ip=request.remote_addr)
 	
-@app.route("/oral_exam_list")
+@app.route("/oral_exam_list.html")
 def oralExamList():
     return render_template("oral_exam_list.html", ip=request.remote_addr)
 
